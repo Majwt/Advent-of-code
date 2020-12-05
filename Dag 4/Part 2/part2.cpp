be2c4ea85e5eeb376bbf346dffa9bed64f5825b1
Majwt <theodor@familjenwase.se>
2020-12-05 12:36:05 +0100

    aaaaaaaaaahhhhhh

--- Dag 4/Part 2/part2.cpp
+++ Dag 4/Part 2/part2.cpp
@@ -54,15 +54,16 @@ string_code hashit(std::string const &inString)
     if (inString == "cid")
         return cid;
 }
-
+int count = 0;
 void checkpassport(passport &data2)
 {
     
     bool dateValid;
     bool hightValid;
     bool hairColourValid;
+    bool eyeColourValid;
     bool idValid;
-    
+    std::cout << std::boolalpha;
     if (!data2.byr.empty() &&
         !data2.iyr.empty() &&
         !data2.eyr.empty() &&
@@ -79,20 +80,49 @@ void checkpassport(passport &data2)
         if ((bir >= 1920 && bir <=2002) && (isu >= 2010 && isu <= 2020) && (exp >= 2020 && exp <= 2030)) 
         {dateValid = true;} else {dateValid = false;}
 
-        std::string s (data2.hcl);
-        std::smatch m;
-        std::regex e ("(#)([a-f0-9])+\n");
+        std::string sh (data2.hcl); std::smatch mh; std::regex eh ("#[a-f\\d]{6}$");
+        while(std::regex_search(sh,mh,eh)) { hairColourValid = mh.ready(); break; }
+
+        std::string se (data2.ecl); std::smatch me; std::regex ee ("(amb|blu|brn|gry|grn|hzl|oth)$");
+        while(std::regex_search(se,me,ee)) { eyeColourValid = me.ready(); break; }
         
-        if(std::regex_match(data2.hcl, std::regex("(#)([a-f0-9])+\n") )) {hairColourValid = true; valid += 200;} else {hairColourValid = false;}
-    
-      valid++;  
+        std::string si (data2.pid); std::smatch mi; std::regex ei ("\\d{9}");
+        while(std::regex_search(si,mi,ei)) { idValid = mi.ready(); break; }
+       
+       std::string st (data2.hgt); std::smatch mt; std::regex et ("((1[5-8]\\d|19[0-3])cm)|((59|6\\d|7[0-6])in)$");
+        while(std::regex_search(st,mt,et)) { hightValid = mt.ready(); break; }
     }
+    //valid++;  
     
-    if (hairColourValid && dateValid) {
-std::cout <<"\n\n" <<"true" << "\n\n";
-valid++;
+    if (hairColourValid &&
+        eyeColourValid &&
+        dateValid &&
+        idValid &&
+        hightValid
+        ) {
+            count++;
+    valid++;
+    std::cout <<"\n\n hColour: " <<hairColourValid <<" = " << data2.hcl << "\n";
+    std::cout <<"\n eColour: " <<eyeColourValid << " = " << data2.ecl << "\n";
+    std::cout <<"\n DateVal: " <<dateValid << " = " << data2.byr << ", " << data2.iyr << ", " << data2.eyr << "\n";
+    std::cout <<"\n IDvalid: " <<idValid << " = " << data2.pid << "\n";
+    std::cout <<"\n HightVa: " <<hightValid << " = " << data2.hgt << "\n\n";
+    std::cout << "valid Count: " << valid << "\n";
+    std::cout <<"nr: " << count << "\n\n";
     data.push_back(data2);
+    } else
+    {
+        count++;
+    std::cout <<"\n\n hColour: " <<hairColourValid <<" = " << data2.hcl << "\n";
+    std::cout <<"\n eColour: " <<eyeColourValid << " = " << data2.ecl << "\n";
+    std::cout <<"\n DateVal: " <<dateValid << " = " << data2.byr << ", " << data2.iyr << ", " << data2.eyr << "\n";
+    std::cout <<"\n IDvalid: " <<idValid << " = " << data2.pid << "\n";
+    std::cout <<"\n HightVa: " <<hightValid << " = " << data2.hgt << "\n\n";
+    std::cout <<"nr: " << count << "\n\n";
+        data.push_back(data2);
     }
+
+    
 }
 
 
@@ -101,7 +131,8 @@ int main()
 {
 
     // std::ifstream input{"d:\\Kod\\Advent of Code\\Dag 4\\example.txt"};
-    std::ifstream input{"d:\\Kod\\Advent of Code\\Dag 4\\input.txt"};
+    // std::ifstream input{"d:\\Kod\\Advent of Code\\Dag 4\\example2.txt"};
+     std::ifstream input{"d:\\Kod\\Advent of Code\\Dag 4\\input.txt"};
     if (!input.is_open())
     {
         return 1;
@@ -184,21 +215,21 @@ int main()
             }
         }
     }
-    for (size_t b = 0; b < data.size(); b++)
-    {
+    // for (size_t b = 0; b < data.size(); b++)
+    // {
         
-        std::cout << "\n" << data.size() << "\n";
-        std::cout << "byr,"<< data[b].byr <<"\n";
-        std::cout << "iyr,"<< data[b].iyr <<"\n";
-        std::cout << "eyr,"<< data[b].eyr <<"\n";
-        std::cout << "hgt,"<< data[b].hgt <<"\n";
-        std::cout << "hcl,"<< data[b].hcl <<"\n";
-        std::cout << "ecl,"<< data[b].ecl <<"\n";
-        std::cout << "pid,"<< data[b].pid <<"\n";
-        std::cout << "cid,"<< data[b].cid <<"\n";
-    }
+    //     std::cout << "\n" << data.size() << "\n";
+    //     std::cout << "byr,"<< data[b].byr <<"\n";
+    //     std::cout << "iyr,"<< data[b].iyr <<"\n";
+    //     std::cout << "eyr,"<< data[b].eyr <<"\n";
+    //     std::cout << "hgt,"<< data[b].hgt <<"\n";
+    //     std::cout << "hcl,"<< data[b].hcl <<"\n";
+    //     std::cout << "ecl,"<< data[b].ecl <<"\n";
+    //     std::cout << "pid,"<< data[b].pid <<"\n";
+    //     std::cout << "cid,"<< data[b].cid <<"\n";
+    // }
     std::cout << "\n"
               << valid << " valid passports";
 std::cout << "\n"
-              << data.size() << " valid passports";
+              << data.size() << "total passports";
 }
