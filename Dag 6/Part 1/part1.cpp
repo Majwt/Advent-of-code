bb7d4cf85452700844261cfe6932fe81a0b96116
Theodor Wase <theodor.wase@learnet.se>
2020-12-07 16:13:53 +0100

    part 1 klarad

--- Dag 6/Part 1/part1.cpp
+++ Dag 6/Part 1/part1.cpp
@@ -10,9 +10,34 @@ std::string line;
 std::vector<std::string> list;
 std::string Path = "..//input.txt";
 // std::string Path = "..//example.txt"; // andra fil vägen
+char alphabet[26] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+int group(std::string searchline,char ch) {
+        if(searchline.find_first_of(ch) != std::string::npos) {
+            return 1;
+        } else
+        {
+            return 0;
+        }
+        
+}
+int myFunction() {
+    int groupTotal = 0;
+    for (size_t n = 0; n < list.size(); n++)
+    {
+        for (size_t p = 0; p < 27; p++)
+        {
+        groupTotal += group(list[n],alphabet[p]);
+            
+        }
+        
+    }
+    return groupTotal;
+    
+}
 int main()
 {
-
+    int total;
     std::ifstream input;
     input.open(Path);
     if (!input.is_open())
@@ -33,5 +58,12 @@ int main()
         }
     }
 
-    ;
+    total += myFunction();
+
+    for (size_t i = 0; i < list.size() + 1; i++)
+    {
+        std::cout << list[i] << "\n";
+    }
+    std::cout <<"\n" << total << " total\n";
+    
 }
