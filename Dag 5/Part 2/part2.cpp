694137918ad0b360e6fc1b7b6bb2eee62aac9906
Theodor Wase <theodor.wase@learnet.se>
2020-12-07 12:53:13 +0100

    fixade formatering

--- Dag 5/Part 2/part2.cpp
+++ Dag 5/Part 2/part2.cpp
@@ -5,7 +5,6 @@
 #include <filesystem>
 #include <algorithm>
 
-
 std::string line;
 int output[851];
 int biggest;
@@ -22,22 +21,18 @@ int myFunction(std::string l)
         if (l.at(i) == 'B')
         {
             arr[i] = 1;
-            // std::cout << "1";
         }
         else if (l.at(i) == 'F')
         {
             arr[i] = 0;
-            // std::cout << "0";
         }
         else if (l.at(i) == 'L')
         {
             arr[i] = 0;
-            // std::cout << " 0";
         }
         else if (l.at(i) == 'R')
         {
             arr[i] = 1;
-            // std::cout << " 1";
         }
 
         i++;
@@ -52,15 +47,14 @@ int myFunction(std::string l)
     num += (arr[6] * 0b10);
     int num2 = (arr[7] * 0b100) + (arr[8] * 0b10) + (arr[9] * 0b1);
     num /= 2;
-     std::cout << " row: " << num << "  coloum: " << num2 << " id: " << (num*8)+num2 << "\n";
-    return (num*8)+num2;
+    std::cout << " row: " << num << "  coloum: " << num2 << " id: " << (num * 8) + num2 << "\n";
+    return (num * 8) + num2;
 }
 int main()
 {
 
     std::ifstream input;
     input.open("input.txt");
-    // ifstream input{"D:\\project\\Advent of Code\\Dag 5\\example.txt"};
     if (!input.is_open())
     {
         return 1;
@@ -68,33 +62,25 @@ int main()
     int counter = 0;
     while (getline(input, line))
     {
-        std::cout<< "\n" << line;
-        
+        std::cout << line << '\n';
         output[counter] = myFunction(line);
         counter++;
     }
-    
+
     int h = sizeof(output) / sizeof(output[0]);
- 
-    /*Here we take two parameters, the beginning of the
-    array and the length n upto which we want the array to
-    be sorted*/
+
     std::sort(output, output + 851);
     for (size_t k = 0; k < 852; k++)
     {
-        if (output[k] - output[k-1] == 1 && output[k] > 0)
+        if (output[k] - output[k - 1] == 1 && output[k] > 0)
         {
-            /* code */
+
             std::cout << output[k] << "\n";
         }
         else
         {
-            std::cout << output[k]-1 <<" THIS ONE HERE\n";
+            std::cout << " THIS ONE HERE" << output[k] - 1 << "\n";
             std::cout << output[k] << "\n";
         }
-        
-        
     }
-    
-    std::cout << biggest;
 }
