c06b1336abd6591f4ea11dc10464991cc7107916
Theodor Wase <theodor.wase@learnet.se>
2020-12-08 07:57:46 +0100

    började dag 8

---
+++ Dag 8/Part 1/part1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <algorithm>
+
+std::string l;
+std::string Path = "..//input.txt";
+// std::string Path = "..//example.txt"; // andra fil vägen
+int main()
+{
+
+    std::ifstream input;
+    input.open(Path);
+    if (!input.is_open())
+    {
+        std::cout << "not found or could not open\n";
+        return 1;
+    }
+
+    while (getline(input, l))
+    {
+        std::cout << l << '\n';
+
+    }
+}
