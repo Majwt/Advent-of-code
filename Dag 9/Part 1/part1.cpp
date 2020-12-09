384398488d38791bc368d8fd75cabc7d9c7339ba
Majwt <theodor@familjenwase.se>
2020-12-09 17:02:32 +0100

    Dag 9 klarad

--- Dag 9/Part 1/part1.cpp
+++ Dag 9/Part 1/part1.cpp
@@ -7,42 +7,51 @@
 
 std::string l;
 std::vector<long long> arr;
-//std::string Path = "..//input.txt";
-std::string Path = "..//example.txt"; // andra fil vägen
+std::string Path = "..//input.txt";
+//std::string Path = "..//example.txt"; // andra fil vägen
 
-int preamable = 5;
-bool myFunction(size_t i) {
-    bool yes;
-    
-    std::cout << i << '\n';
-    
-    
-        for (size_t n = i-1; n > i-(preamable+1); n--)
+int inputnum = 25;
+
+bool myFunction(size_t i)
+{
+    bool yes = false;
+
+    for (size_t n = i - inputnum; n < i - 0 ; n++)
+    {
+
+        for (size_t v = i - inputnum; v < i ; v++)
         {
+            if(v == n) {
+                continue;
+            }
+            if (arr[n] + arr[v] == arr[i])
+            {
+                // std::cout << "O | "<< n <<"\t"<< v <<"\t| "
+                //           << arr[n] << "\t+\t" << arr[v] << "\t =\t" << arr[n] + arr[v] << "\t| " << arr[i] << "\n";
+                 yes = true;
+                 //break;
+                 goto skip;
+                // return yes;
+            }
+            else
+            {
+                
+                // std::cout << "x | "<< n << "\t" << v <<"\t| "
+                //           << arr[n] << "\t+\t" << arr[v] << "\t =\t" << arr[n] + arr[v] << "\t| " << arr[i] << "\n";
+                
+            }
+        }
         
-        for (size_t v = i-(preamable+1); v < i-1; v++)
-        {
-        
-        if (arr[i] == arr[n]+arr[v] && arr[n] != arr[v]) {
-            yes = true;            
-            break;
-        }
-            std::cout << "no v\n";
-        }
-            std::cout << "no n \n";
-
-        }
-    
-    
-    if (yes) {
-            return true;
-        }
-        else 
-        {
-
+    }
+    std::cout << "\nLine: " << i+1 << std::endl;
+    if(yes) {
+    skip:
+        return true;
+    } else {
         return false;
-        }
-    
+    }
+
+
 }
 
 int main()
@@ -52,25 +61,33 @@ int main()
     input.open(Path);
     if (!input.is_open())
     {
-        std::cout << "not found or could not open\n";
+        std::cout << "not found or could not open" << std::endl;
         return 1;
     }
 
     while (getline(input, l))
     {
-        long long number = std::stoll(l,nullptr,10);
-        std::cout << number << '\n';
-         arr.push_back(number);
+        long long number = std::stoll(l, nullptr, 10);
+        //std::cout << number << '\n';
+        arr.push_back(number);
     }
-
-    for (size_t i = preamable+1; i < arr.size(); i++)
+    std::vector<int> fake;
+    for (size_t i = inputnum; i < arr.size(); i++)
     {
-       if(myFunction(i) == false) {
-           std::cout << "\nindex: " << i << '\n';
-           break;
-       } else {
-           continue;
-       }
+        std::boolalpha;
+        
+        if (!myFunction(i))
+        {
+            
+            //std::cout << "\nFalse\n--------------------\n\n\n";
+            fake.push_back(i+1);
+            goto slut;
+            
+            continue;
+        }
+      
     }
-    
+    slut:
+    std::cout << "done" << std::endl;
+    std::cout << "first false: " << arr[fake[0]-1];
 }
--- Dag 9/Part 2/part2.cpp
+++ Dag 9/Part 2/part2.cpp
@@ -6,22 +6,61 @@
 #include <algorithm>
 
 std::string l;
+std::vector<long long> arr;
+std::vector<long long> fake;
 std::string Path = "..//input.txt";
-// std::string Path = "..//example.txt"; // andra fil vägen
+//std::string Path = "..//example.txt"; // andra fil vägen
+
+
+bool myFunction()
+{
+    bool yes = false;
+
+    for (size_t n = 0; n < arr.size(); n++)
+    {
+
+        long long testing = arr[n];
+
+        std::cout << "\n\n";
+        std::cout << arr[n] << std::endl;
+
+        for (size_t v = 1; v < arr.size(); v++)
+        {
+            std::cout << arr[n + v] << std::endl;
+            testing += arr[n + v];
+
+            if (testing == 1492208709)
+            {
+                std::cout << "\n"<< testing << std::endl;
+                yes = true;
+                goto skip;
+            }
+        }
+    }
+    if (yes) {skip:return true;} else {return false;}
+}
+
 int main()
 {
-
     std::ifstream input;
     input.open(Path);
     if (!input.is_open())
     {
-        std::cout << "not found or could not open\n";
+        std::cout << "not found or could not open" << std::endl;
         return 1;
     }
-
     while (getline(input, l))
     {
-        std::cout << l << '\n';
-
+        long long number = std::stoll(l, nullptr, 10);
+        arr.push_back(number);
+    }
+    std::boolalpha;
+    if (myFunction())
+    {
+        std::cout << "\ndone\n"<< std::endl;
+    }
+    else
+    {
+        std::cout << "didnt find" << std::endl;
     }
 }
