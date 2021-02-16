1e418cb242380c709984aac63c2ebeb1ac5e747f
Theodor Wase <theodor@familjenwase.se>
2021-02-16 09:51:08 +0100

    gav up

--- Dag 23/Del 2/part2dict.cpp
+++ Dag 23/Del 2/part2dict.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 // std::string Path = "..//input.txt";
 std::string Path = "..//example.txt"; // andra fil vägen
-vector<int> temp{3,8,9,1,2,5,4,6,7};
-map<int,int> circle;
+vector<int> temp{3, 8, 9, 1, 2, 5, 4, 6, 7};
+map<int, int> circle;
 
 struct node
 {
     int data;
-    struct node* next;
+    struct node *next;
 };
 
 int to_int(char i)
@@ -21,7 +21,7 @@ int to_int(char i)
 int mod(int a, int b)
 {
     int c = a % b;
-    return (c < 0) ? c + b : c;
+    return (c <= 0) ? c + b : c;
 }
 
 namespace std::chrono
@@ -33,7 +33,7 @@ namespace std::chrono
      * @param stop stop time
      * @param suff 1 = minuets, 2 = seconds, 3 = milliseconds, 4 = microseconds, 5 = nanoseconds
      */
-    void get_time(steady_clock::time_point start, steady_clock::time_point stop, int suff = 1)
+    void get_timeDiff(steady_clock::time_point start, steady_clock::time_point stop, int suff = 1)
     {
         int64_t t;
         switch (suff)
@@ -69,13 +69,12 @@ namespace std::chrono
         }
     }
 
-};
+}; // namespace std::chrono
 
+struct node *add_end(struct node *tail, int data)
+{
 
-
-struct node* add_end(struct node* tail,int data) {
-
-    node* temp = new node;
+    node *temp = new node;
     temp->data = data;
     temp->next = NULL;
 
@@ -85,35 +84,191 @@ struct node* add_end(struct node* tail,int data) {
     return tail;
 }
 
-void print(node* tail) {
-    node* p = tail->next;
+void print(node *tail, int current = 0)
+{
+    node *p = tail->next;
     do
     {
-        printf("%d ",p->data);
+        if (p == tail->next)
+        {
+            (p->data == current) ? printf("(%d)", p->data) : printf("%d ", p->data);
+        }
+        else
+        {
+            (p->data == current) ? printf("(%d)", p->data) : printf(" %d ", p->data);
+        }
         p = p->next;
-    } while (p!=tail->next);
+    } while (p != tail->next);
+    cout << endl;
+}
 
+struct node *insertAfter(node *last, int new_data, int after_item)
+{
+    //return null if list is empty
+    if (last == NULL)
+        return NULL;
+    after_item = mod(after_item, 10);
+    node *temp, *p;
+    p = last->next;
+
+    do
+    {
+        if (p->data == after_item)
+        {
+            temp = new node;
+            temp->data = new_data;
+            temp->next = p->next;
+            p->next = temp;
+
+            if (p == last)
+                last = temp;
+            // printf("destination: %d\n", after_item);
+            return last;
+        }
+        p = p->next;
+    } while (p != last->next);
+    // cout << "The node with data " << after_item << " is not present in the list." << endl;
+
+    return insertAfter(last, new_data, after_item - 1);
+}
+
+vector<int> arr;
+int deleteNode(node *head, int key)
+{
+
+    // If linked list is empty
+    if (head == NULL)
+        return -1;
+
+    // If the list contains only a single node
+    if (head->data == key && head->next == head)
+    {
+        free(head);
+        head = NULL;
+        return -1;
+    }
+
+    node *last = head, *d;
+
+    // If head is to be deleted
+    if (head->data == key)
+    {
+        cout << "head == key\t" << head->data << endl;
+        // Find the last node of the list
+        // while (last->next != head)
+        //     last = last->next;
+
+        // Point last node to the next of head i.e.
+        // the second node of the list
+        last->next = head->next;
+        int o = head->data;
+        free(head);
+        head = last->next;
+        return o;
+    }
+
+    // Either the node to be deleted is not found
+    // or the end of list is not reached
+    // while (last->next != head && last->next->data != key)
+    // {
+    //     last = last->next;
+    // }
+
+    // If node to be deleted was found
+    if (last->next->data == key)
+    {
+
+        d = last->next;
+        last->next = d->next;
+        int o = d->data;
+        free(d);
+
+        return o;
+    }
+    else
+    {
+
+        cout << "no such keyfound";
+        return -1;
+    }
 }
 
 int main()
 {
+    // vector<int> input = {3,1,5,6,7,9,8,2,4};
+    vector<int> input = {3,8,9,1,2,5,4,6,7};
     cout << "h";
-    node* tail = new node;
+    node *tail = new node;
     cout << "o\n";
-
-    tail->data = 3;
+    tail->data = input[0];
     tail->next = tail;
-    tail = add_end(tail,8);
-    tail = add_end(tail,9);
-    tail = add_end(tail,1);
-    tail = add_end(tail,2);
-    tail = add_end(tail,5);
-    tail = add_end(tail,4);
-    tail = add_end(tail,6);
-    tail = add_end(tail,7);
-    print(tail);    
+    node *curr = tail;
+    for (size_t i = 1; i < input.size(); i++)
+    {
+    tail = add_end(tail, input[i]);
+
+    }
+
+    for (size_t i = input.size(); i < 1000000; i++)
+    {
+        tail = add_end(tail, i);
+    }
     
     
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        /* code */
+
+        printf("circle | %d | curr = %d\n", i, curr->data);
+        // print(curr, curr->data);
+        // cout << curr->data << endl;
+        auto o1 = chrono::steady_clock::now();
+        int dataTemp = curr->data;
+        vector<int> deled;
+        deled = {};
+        cout << "cups: ";
+        // print(curr, curr->data);
+        for (size_t k = 0; k < 3; k++)
+        {
+            int de = deleteNode(curr, curr->next->data);
+            deled.push_back(de);
+        }
+        // printf("holding: ");
+        // for (auto &&j : deled)
+        // {
+        //     printf("%d ", j);
+        // }
+        // printf("\ndest pre: %d\n", dataTemp - 1);
+        node *t = curr->next;
+        insertAfter(curr, deled[0], dataTemp - 1);
+        insertAfter(curr, deled[1], deled[0]);
+        insertAfter(curr, deled[2], deled[1]);
+        // print(curr, dataTemp);
+        curr = t;
+        auto o2 = chrono::steady_clock::now();
+        get_timeDiff(o1,o2,3);
+        // cout << endl;
+    }
+
+    print(curr, 1);
+
+    long long s1;
+    long long s2;
+    node *p = curr->next;
+    do
+    {
+        if (p->data == 1) {
+
+        printf(" ");
+        s1 = p->next->data;
+        s2 = p->next->next->data;
+        } else {
+        printf("%d", p->data);
+
+        }
+        p = p->next;
+    } while (p != curr->next);
+        printf("\n\t%d\n*\t%d\n=\n%d\n",s1,s2,(s1*s2));
+    cout << endl;
 }
-
-
